89221b9d6dcc3c5e336d21ad9083d2383488c757
Sanarix <KorsakovLA@mail.ru>
2025-11-07 17:19:02 +0300

    change app with no-firebase

--- src/App.js
+++ src/App.js
@@ -2,11 +2,8 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
-import { initializeApp } from "firebase/app";
-import firebaseConfig from './config/firebaseConfig.json'
 
 function App() {
-  initializeApp(firebaseConfig);
   return (
     <>
       <Header />
