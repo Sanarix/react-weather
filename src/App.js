904c161328450ea5f368486b60c893652d4df730
Sanarix <KorsakovLA@mail.ru>
2025-11-07 17:39:00 +0300

    revert pull-request

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from './config/firebaseConfig.json'
 
 function App() {
+  initializeApp(firebaseConfig);
   return (
     <>
       <Header />
